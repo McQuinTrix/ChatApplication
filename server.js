4defece3c1064bf6832dbf19ec22c93d7e9972a4
Harshal <harshal.carpenter@gmail.com>
2015-04-14 16:22:46 -0700

    Update server.js
    New Server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express 	= require("express"),
 	app 	= express(),
 	server	= require('http').createServer(app),
-	io	= require('socket.io').listen(server);
+	io	= require('socket.io').listen(server),
+	nicknames= [];
 	// the socket object listens to http objects
 
 server.listen(3000);
@@ -11,9 +12,29 @@ app.get('/',function(req,res){
 });
 
 io.sockets.on('connection',function(socket){
+	socket.on('new user',function(data,callback){
+		if(nicknames.indexOf(data) != -1){
+			callback(false);
+		}else{
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			io.sockets.emit('usernames', nicknames);
+			updateNicks();
+		}
+	});
+	function updateNicks(){
+		io.sockets.emit('usernames', nicknames);
+	}
 	socket.on('send message', function(data){
-		io.sockets.emit('new message',data);
+		io.sockets.emit('new message',{msg: data, nick: socket.nickname});
 		//socket.broadcast.emit('new message', data);
+	});
+	socket.on('disconnect',function(data){
+		if(!socket.nickname) return;
+		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		updateNicks();
+		io.sockets.emit('disco',{nick: socket.nickname});
 	});
 });
 
