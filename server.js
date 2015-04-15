e23ae6102c48c0368dafe37985b821eac538e242
Harshal <harshal.carpenter@gmail.com>
2015-04-14 22:53:08 -0700

    Update server.js
    After Part 3 of the video <additional functions added>

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express 	= require("express"),
 	app 	= express(),
 	server	= require('http').createServer(app),
 	io	= require('socket.io').listen(server),
-	nicknames= [];
+	users = {};
 	// the socket object listens to http objects
 
-server.listen(3000);
+server.listen(3001);
 
 app.get('/',function(req,res){
 	res.sendfile(__dirname+'/index.html');
@@ -13,27 +13,41 @@ app.get('/',function(req,res){
 
 io.sockets.on('connection',function(socket){
 	socket.on('new user',function(data,callback){
-		if(nicknames.indexOf(data) != -1){
+		if(data in users){
 			callback(false);
 		}else{
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateNicks();
 		}
 	});
 	function updateNicks(){
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
-	socket.on('send message', function(data){
-		io.sockets.emit('new message',{msg: data, nick: socket.nickname});
-		//socket.broadcast.emit('new message', data);
+	socket.on('send message', function(data,callback){
+		var msg = data.trim();
+		if(msg.substr(0,3) == '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(" ");
+			if(ind !== -1){
+				var name = msg.substring(0,ind);
+				var msg = msg.substring(ind+1);
+				console.log();
+				if(name in users){
+					users[name].emit('whisper',{msg: msg, nick: socket.nickname});console.log("Whisper");
+				}else{callback('Enter valid name!');}
+			}else{
+				callback('Please put a message in correct format!');
+			}
+		}else{
+			io.sockets.emit('new message',{msg: data, nick: socket.nickname});
+		}//socket.broadcast.emit('new message', data);
 	});
 	socket.on('disconnect',function(data){
 		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname),1);
+		delete users[socket.nickname];
 		updateNicks();
 		io.sockets.emit('disco',{nick: socket.nickname});
 	});
 });
-
